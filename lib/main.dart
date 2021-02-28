75316e2af145a062c3af970e6afc8d5e0397ee4b
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-28 13:54:31 +0600

    chaged home ui

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'NewsApp version2',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
